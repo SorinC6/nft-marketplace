2d1b4bb46e4a3bd6c54b6b69f5f75651c23b0a81
SorinC6 <kish.sorin@yahoo.com>
2021-09-22 20:57:45 +0300

    deploy to mumbai test network

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-
-  export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftmarketaddress = '0xcFFd89a3DD3e762ED75B677eD3F777ABB9196585';
+export const nftaddress = '0x8e146D2cEaF10c4BDd9cdbCDAa322c92207664cB';
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // https://hardhat.org/guides/create-task.html
 // Go to https://hardhat.org/config/
 
-const privateKey = fs.readFileSync('.secret').toString().trim() || '';
+const privateKey = fs.readFileSync('.secretid').toString().trim() || '';
 const infuraId = fs.readFileSync('.infuraid').toString().trim() || '';
 
 module.exports = {
@@ -12,11 +12,13 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
+      // url: 'https://ipfs.infura.io:5001/1yOy5GWLiswH7hPZu3mEjvsd2wG',
+      // url: 'https://polygon-mumbai.infura.io/v3/1270e7238e4e49e68bd723b96f2262df',
       url: 'https://polygon-mumbai.infura.io/v3/1270e7238e4e49e68bd723b96f2262df',
       accounts: [privateKey],
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: `https://polygon-mainnet.infura.io/v3/1270e7238e4e49e68bd723b96f2262df`,
       accounts: [privateKey],
     },
   },
